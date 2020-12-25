46c04c4f9728495254a921970a2b8c1815aed673
phoebe0109 <71374909+phoebe0109@users.noreply.github.com>
2020-12-25 20:28:36 +0800

    Update tree.cpp

--- homework/tree.cpp
+++ homework/tree.cpp
@@ -6,9 +6,74 @@ struct node
     char data;
     struct node* left;
     struct node* right;
-};
-
-void preorder(node* c)
+}
+void breadthFirst(node*);
+void preorder(node*);
+void inorder(node*);
+void postorder(node*);
+int main(void){
+ 
+  node *root,*p1,*p2,*p3,*p4,*p5,*p6,*p7;
+  p1=new node;
+  p1->data='1';
+  root=p1;
+  p2=new node;
+  p2->data='2';  
+  p3=new node;
+  p3->data='3';
+  p4=new node;
+  p4->data='4';
+  p5=new node;
+  p5->data='5';
+  p6=new node;
+  p6->data='6';
+  p7=new node;
+  p7->data='7';
+  p1->left=p2;
+  p1->right=p3;
+  p2->left=p4;
+  p2->right=p5;
+  p3->left=p6;
+  p3->right=p7;
+  p4->left=NULL;
+  p4->right=NULL;
+  p5->left=NULL;
+  p5->right=NULL;
+  p6->left=NULL;
+  p6->right=NULL;
+  p7->left=NULL;
+  p7->right=NULL;
+  preorder(root);
+  cout << endl;
+  inorder(root);
+  cout << endl;
+  postorder(root);
+  cout << endl;
+}
+void
+void preorder(node *p){
+  if(p) {
+    cout << p->data << " ";
+    preorder(p->left);
+    preorder(p->right);
+  }
+}
+void inorder(node *p){
+  if(p) {
+    inorder(p->left);
+    cout << p->data << " ";
+    inorder(p->right);
+  }
+}
+void postorder(node *p){
+  if(p) {
+    postorder(p->left);
+    postorder(p->right);
+    cout << p->data << " ";
+  }
+}
+/*v0.1
+  void preorder(node* c)
 {
     if (c)
     {
